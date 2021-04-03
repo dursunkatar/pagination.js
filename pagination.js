4323e77963a5f69ae7c276d54143925ed72ab734
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2021-04-03 18:19:43 +0300

    Add files via upload

---
+++ pagination.js
@@ -0,0 +1,90 @@
+/* *********************************
+ *          Dursun Katar           *
+ *         dursunkatar.com         *
+ *       github.com/dursunkatar    *
+ *                                 *
+ ***********************************/
+var PaginationConfig = /** @class */ (function () {
+    function PaginationConfig() {
+    }
+    return PaginationConfig;
+}());
+var Pagination = /** @class */ (function () {
+    function Pagination(config) {
+        this.config = config;
+        this.pageButtons = [];
+        this.init();
+    }
+    Pagination.prototype.init = function () {
+        this.pageButtonCount = Math.ceil(this.config.TotalItemCount / this.config.PageShowItemSize);
+        this.config.MaxShowButtonSize = this.config.MaxShowButtonSize > this.pageButtonCount ? this.pageButtonCount : this.config.MaxShowButtonSize;
+        this.addLeftRightButtonCount = Math.ceil((this.config.MaxShowButtonSize - 1) / 2);
+    };
+    Pagination.SetConfig = function (configCallBack) {
+        var config = new PaginationConfig();
+        configCallBack(config);
+        var pagination = new Pagination(config);
+        return pagination;
+    };
+    Pagination.prototype.Invoke = function (pageIndex) {
+        if (pageIndex === void 0) { pageIndex = 1; }
+        this.currentPageIndex = pageIndex;
+        var leftRsidual = pageIndex - this.addLeftRightButtonCount;
+        this.addLeftButtonNumbers(pageIndex);
+        if (this.pageButtonCount - pageIndex < this.addLeftRightButtonCount) {
+            this.addRightRsidualPageNumber(pageIndex);
+        }
+        this.pageButtons.push(pageIndex);
+        for (var i = leftRsidual; i < 1; i++) {
+            this.pageButtons.push(++pageIndex);
+        }
+        this.addRigtButtonBumbers(pageIndex);
+        this.build(this.pageButtons);
+    };
+    Pagination.prototype.build = function (arr) {
+        var url = this.config.Url;
+        var parent = document.getElementById(this.config.ParentDivId);
+        if (this.currentPageIndex > 1) {
+            parent.insertAdjacentHTML('beforeend', this.config.ArrowLeftHtml.replace('{0}', "" + url + (this.currentPageIndex - 1)));
+        }
+        for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+            var pageIndex = arr_1[_i];
+            parent.insertAdjacentHTML('beforeend', "<a class=\"" + (this.currentPageIndex === pageIndex ? 'active' : '') + "\" href=\"" + url + pageIndex + "\">" + pageIndex + "</a>");
+        }
+        if (this.currentPageIndex + 1 < this.pageButtonCount) {
+            parent.insertAdjacentHTML('beforeend', this.config.ArrowRightHtml.replace('{0}', "" + url + (this.currentPageIndex + 1)));
+        }
+    };
+    Pagination.prototype.addRightRsidualPageNumber = function (pageIndex) {
+        var rightRsidual = this.addLeftRightButtonCount - (this.pageButtonCount - pageIndex);
+        var firstNumber = this.pageButtons[0];
+        for (var i = 0; i < rightRsidual; i++) {
+            if (--firstNumber === 0) {
+                return;
+            }
+            this.pageButtons.unshift(firstNumber);
+        }
+    };
+    Pagination.prototype.addRigtButtonBumbers = function (pageIndex) {
+        for (var i = 0; i < this.addLeftRightButtonCount; i++) {
+            if (++pageIndex > this.pageButtonCount) {
+                return;
+            }
+            console.log("pageIndex=" + pageIndex);
+            this.pageButtons.push(pageIndex);
+        }
+    };
+    Pagination.prototype.addLeftButtonNumbers = function (pageIndex) {
+        var arr = [];
+        for (var i = 0; i < this.addLeftRightButtonCount; i++) {
+            if (--pageIndex === 0) {
+                break;
+            }
+            arr.push(pageIndex);
+        }
+        arr.reverse();
+        this.pageButtons = this.pageButtons.concat(arr);
+    };
+    return Pagination;
+}());
+//# sourceMappingURL=pagination.js.map
---
+++ pagination.ts
@@ -0,0 +1,100 @@
+﻿/* *********************************
+ *          Dursun Katar           *
+ *         dursunkatar.com         *
+ *       github.com/dursunkatar    *
+ *                                 *
+ ***********************************/
+
+class PaginationConfig {
+    public Url: string;
+    public ParentDivId: string;
+    public ArrowLeftHtml: string;
+    public ArrowRightHtml: string;
+    public TotalItemCount: number;
+    public PageShowItemSize: number;
+    public MaxShowButtonSize: number;
+}
+
+class Pagination {
+    private pageButtonCount: number;
+    private addLeftRightButtonCount: number;
+    private pageButtons: number[] = [];
+    private currentPageIndex: number;
+
+    constructor(private config: PaginationConfig) {
+        this.init();
+    }
+
+    private init(): void {
+        this.pageButtonCount = Math.ceil(this.config.TotalItemCount / this.config.PageShowItemSize);
+        this.config.MaxShowButtonSize = this.config.MaxShowButtonSize > this.pageButtonCount ? this.pageButtonCount : this.config.MaxShowButtonSize;
+        this.addLeftRightButtonCount = Math.ceil((this.config.MaxShowButtonSize - 1) / 2);
+    }
+    public static SetConfig(configCallBack: (config: PaginationConfig) => void): Pagination {
+        const config = new PaginationConfig();
+        configCallBack(config);
+        const pagination = new Pagination(config);
+        return pagination;
+    }
+    public Invoke(pageIndex: number = 1) {
+        this.currentPageIndex = pageIndex;
+        const leftRsidual = pageIndex - this.addLeftRightButtonCount;
+        this.addLeftButtonNumbers(pageIndex);
+
+        if (this.pageButtonCount - pageIndex < this.addLeftRightButtonCount) {
+            this.addRightRsidualPageNumber(pageIndex);
+        }
+        this.pageButtons.push(pageIndex);
+
+        for (var i = leftRsidual; i < 1; i++) {
+            this.pageButtons.push(++pageIndex);
+        }
+
+        this.addRigtButtonBumbers(pageIndex);
+        this.build(this.pageButtons);
+    }
+    private build(arr: number[]): void {
+        const url = this.config.Url;
+        const parent = document.getElementById(this.config.ParentDivId);
+        if (this.currentPageIndex > 1) {
+            parent.insertAdjacentHTML('beforeend', this.config.ArrowLeftHtml.replace('{0}', `${url}${this.currentPageIndex - 1}`));
+        }
+        for (const pageIndex of arr) {
+            parent.insertAdjacentHTML('beforeend', `<a class="${this.currentPageIndex === pageIndex ? 'active' : ''}" href="${url}${pageIndex}">${pageIndex}</a>`);
+        }
+        if (this.currentPageIndex + 1 < this.pageButtonCount) {
+            parent.insertAdjacentHTML('beforeend', this.config.ArrowRightHtml.replace('{0}', `${url}${this.currentPageIndex + 1}`));
+        }
+    }
+    private addRightRsidualPageNumber(pageIndex: number): void {
+        const rightRsidual = this.addLeftRightButtonCount - (this.pageButtonCount - pageIndex);
+        let firstNumber = this.pageButtons[0];
+        for (var i = 0; i < rightRsidual; i++) {
+            if (--firstNumber === 0) {
+                return;
+            }
+            this.pageButtons.unshift(firstNumber);
+        }
+    }
+    private addRigtButtonBumbers(pageIndex: number): void {
+        for (var i = 0; i < this.addLeftRightButtonCount; i++) {
+            if (++pageIndex > this.pageButtonCount) {
+                return;
+            }
+            console.log(`pageIndex=${pageIndex}`);
+            this.pageButtons.push(pageIndex);
+        }
+    }
+    private addLeftButtonNumbers(pageIndex: number): void {
+        const arr: number[] = [];
+        for (var i = 0; i < this.addLeftRightButtonCount; i++) {
+            if (--pageIndex === 0) {
+                break;
+            }
+            arr.push(pageIndex);
+        }
+        arr.reverse();
+        this.pageButtons = this.pageButtons.concat(arr);
+    }
+}
+
